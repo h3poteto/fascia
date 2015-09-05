b88dc21583eacbfc455eaac848a34c312b45b417
h3poteto <h3.poteto@gmail.com>
2015-09-05 22:19:15 +0900

    refs #2 userモデルのテストをテストDBで実行させる

--- models/db/db.go
+++ models/db/db.go
@@ -15,7 +15,8 @@ type Database struct {
 func (u *Database) Init() *sql.DB {
 	username := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
-	db, err := sql.Open("mysql", username + ":" + password + "@/fascia?charset=utf8")
+	database := os.Getenv("DB_NAME")
+	db, err := sql.Open("mysql", username + ":" + password + "@/" + database + "?charset=utf8")
 	if err != nil {
 		panic(err.Error())
 	}
--- test/user_test.go
+++ test/user_test.go
@@ -1,9 +1,27 @@
 package user
 
 import (
+	"os"
 	"testing"
-	"."
+	"../models/user"
+	"../models/db"
 )
+
+func TestMain(m *testing.M) {
+	// TODO: ここあとで共通化したい
+	testdb := os.Getenv("DB_TEST_NAME")
+	currentdb := os.Getenv("DB_NAME")
+	os.Setenv("DB_NAME", testdb)
+
+	code := m.Run()
+	mydb := &db.Database{}
+	var database db.DB = mydb
+	sql := database.Init()
+	sql.Exec("truncate table users;")
+	sql.Close()
+	os.Setenv("DB_NAME", currentdb)
+	os.Exit(code)
+}
 
 func TestRegistrationAndLogin(t *testing.T) {
 	aUser := user.NewUser()
