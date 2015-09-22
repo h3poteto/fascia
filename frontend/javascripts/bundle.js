458c4165ef29b62fa03b4095495bdb6242389c2e
h3poteto <h3.poteto@gmail.com>
2015-09-23 01:45:58 +0900

    refs #36 基本のメニューバーを作成

--- frontend/javascripts/bundle.js
+++ frontend/javascripts/bundle.js
@@ -48,7 +48,7 @@ var Board = React.createClass({
     render: function() {
         return <div>
             <input type="text" value={this.state.newText} onChange={this.updateNewText} placeholder="Project Name" className="form-control" />
-            <button onClick={this.addItem} className="pure-button pure-button-primary" type="button">CreateProject</button>
+            <button onClick={this.addItem} className="pure-button pure-button-primary fascia-button" type="button">CreateProject</button>
             <div className="items">
                 {this.state.items.map(function(item, index) {
                     return <div className="item" data-id={item.Id}>
@@ -70,3 +70,39 @@ var routes = (
 );
 
 React.render(routes, document.getElementById("board"));
+
+
+var Menu = React.createClass({
+    getInitialState: function() {
+        return {
+            selected: "projects"
+        };
+    },
+    selectMenu: function(selectedClass) {
+        this.setState({
+            selected: selectedClass
+        });
+    },
+    render: function() {
+        return <div className="top-nav">
+            <div className="pure-menu pure-menu-horizontal">
+            <span className="pure-menu-heading">fascia</span>
+            <ul className="pure-menu-list fascia-menu-list">
+            <li className="pure-menu-item fascia-menu-item"><a href="/" className="pure-menu-link">projects</a></li>
+            </ul>
+            <ul className="pure-menu-list fascia-menu-list right-align-list">
+            <li className="pure-menu-item fascia-menu-item"><a href="#" className="pure-menu-link">settings</a></li>
+            </ul>
+            </div>
+            </div>;
+        }
+});
+
+var menuRoutes = (
+    <Router>
+      <Route path="/" component={Menu}>
+      </Route>
+    </Router>
+);
+
+React.render(menuRoutes, document.getElementById("top"));
--- views/layouts/application.html.tpl
+++ views/layouts/application.html.tpl
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <title>{% block title %}Fascia{% endblock %}</title>
     <link rel="stylesheet" href="/stylesheets/pure-min.css" media="all">
+    <link rel="stylesheet" href="/stylesheets/application.css" media="all">
 
 </head>
 <body>
+    <div id="top">
+    </div>
     <div id="content">
         {% block content %}{% endblock %}
     </div>
