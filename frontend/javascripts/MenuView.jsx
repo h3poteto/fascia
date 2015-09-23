8e0729983e7d175e1b6463bd9dde5ef6b4d3d3a5
h3poteto <h3.poteto@gmail.com>
2015-09-23 18:03:12 +0900

    refs #36 メニューのjsもViewとViewModelを分離

---
+++ frontend/javascripts/MenuView.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+var MenuView = function(selected) {
+  return <div className="top-nav">
+    <div className="pure-menu pure-menu-horizontal">
+      <span className="pure-menu-heading fascia-menu-heading">fascia</span>
+      <ul className="pure-menu-list fascia-menu-list">
+        <li className="pure-menu-item fascia-menu-item"><a href="/" className="pure-menu-link">projects</a></li>
+      </ul>
+      <ul className="pure-menu-list fascia-menu-list right-align-list">
+        <li className="pure-menu-item fascia-menu-item pure-menu-has-children pure-menu-allow-hover"><a href="#" className="pure-menu-link">settings</a>
+          <ul className="pure-menu-children">
+            <li className="pure-menu-item fascia-menu-item"><a href="#" className="pure-menu-link">profile</a></li>
+            <li className="pure-menu-item fascia-menu-item"><a href="#" className="pure-menu-link">account</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>;
+
+}
+
+export default MenuView;
--- frontend/javascripts/bundle.js
+++ frontend/javascripts/bundle.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Router, Route, Link} from 'react-router';
 import Request from 'superagent';
 import BoardViewModel from './BoardViewModel';
-
+import MenuViewModel from './MenuViewModel';
 
 // TODO: ある程度できたらreduxで状態管理する
 
@@ -15,41 +15,9 @@ var routes = (
 
 React.render(routes, document.getElementById("board"));
 
-
-var Menu = React.createClass({
-  getInitialState: function() {
-    return {
-      selected: "projects"
-    };
-  },
-  selectMenu: function(selectedClass) {
-    this.setState({
-      selected: selectedClass
-    });
-  },
-  render: function() {
-    return <div className="top-nav">
-      <div className="pure-menu pure-menu-horizontal">
-      <span className="pure-menu-heading fascia-menu-heading">fascia</span>
-      <ul className="pure-menu-list fascia-menu-list">
-      <li className="pure-menu-item fascia-menu-item"><a href="/" className="pure-menu-link">projects</a></li>
-      </ul>
-      <ul className="pure-menu-list fascia-menu-list right-align-list">
-      <li className="pure-menu-item fascia-menu-item pure-menu-has-children pure-menu-allow-hover"><a href="#" className="pure-menu-link">settings</a>
-      <ul className="pure-menu-children">
-      <li className="pure-menu-item fascia-menu-item"><a href="#" className="pure-menu-link">profile</a></li>
-      <li className="pure-menu-item fascia-menu-item"><a href="#" className="pure-menu-link">account</a></li>
-      </ul>
-      </li>
-      </ul>
-      </div>
-      </div>;
-  }
-});
-
 var menuRoutes = (
     <Router>
-    <Route path="/" component={Menu}>
+    <Route path="/" component={MenuViewModel}>
     </Route>
     </Router>
 );
