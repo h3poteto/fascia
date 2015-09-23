9069dfffaab4c3fc4ccedea7f4108c002e5070d8
h3poteto <h3.poteto@gmail.com>
2015-09-23 15:36:51 +0900

    refs #36 メニューの基本デザイン

--- frontend/javascripts/bundle.js
+++ frontend/javascripts/bundle.js
@@ -86,12 +86,17 @@ var Menu = React.createClass({
     render: function() {
         return <div className="top-nav">
             <div className="pure-menu pure-menu-horizontal">
-            <span className="pure-menu-heading">fascia</span>
+            <span className="pure-menu-heading fascia-menu-heading">fascia</span>
             <ul className="pure-menu-list fascia-menu-list">
             <li className="pure-menu-item fascia-menu-item"><a href="/" className="pure-menu-link">projects</a></li>
             </ul>
             <ul className="pure-menu-list fascia-menu-list right-align-list">
-            <li className="pure-menu-item fascia-menu-item"><a href="#" className="pure-menu-link">settings</a></li>
+            <li className="pure-menu-item fascia-menu-item pure-menu-has-children pure-menu-allow-hover"><a href="#" className="pure-menu-link">settings</a>
+            <ul className="pure-menu-children">
+            <li className="pure-menu-item fascia-menu-item"><a href="#" className="pure-menu-link">profile</a></li>
+            <li className="pure-menu-item fascia-menu-item"><a href="#" className="pure-menu-link">account</a></li>
+            </ul>
+            </li>
             </ul>
             </div>
             </div>;
