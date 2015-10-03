449c2f4fcc0c8b2868eb74f5195408ae44342334
h3poteto <h3.poteto@gmail.com>
2015-10-04 02:34:41 +0900

    refs #67 assetsにキャッシュを持たせつつ変更検知させる

--- server.go
+++ server.go
@@ -2,8 +2,13 @@ package main
 import (
 	"flag"
 	"net/http"
+	"fmt"
+	"syscall"
+	"strconv"
 	"github.com/zenazn/goji"
 	"github.com/zenazn/goji/web"
+	"github.com/flosch/pongo2"
+	_ "github.com/flosch/pongo2-addons"
 	"./controllers"
 )
 
@@ -27,7 +32,23 @@ func Routes(m *web.Mux) {
 }
 
 func main() {
+	pongo2.RegisterFilter("suffixAssetsUpdate", SuffixAssetsUpdate)
 	flag.Set("bind", ":9090")
 	Routes(goji.DefaultMux)
 	goji.Serve()
+}
+
+func SuffixAssetsUpdate(in *pongo2.Value, param *pongo2.Value) (out *pongo2.Value, err *pongo2.Error) {
+	assetsFile, ok := in.Interface().(string)
+	if !ok {
+		return nil, &pongo2.Error{
+			Sender: "suffixStylesheet",
+			ErrorMsg: fmt.Sprintf("Data must be string %T ('%v')", in, in),
+		}
+	}
+
+	var file syscall.Stat_t
+	syscall.Stat("./public/assets" + assetsFile, &file)
+	timestamp, _ := file.Mtim.Unix()
+	return pongo2.AsValue(assetsFile + "?update=" + strconv.FormatInt(timestamp, 10)), nil
 }
