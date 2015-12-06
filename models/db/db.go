2d85e28cbdf5d878d937792f609f04021fe74a49
h3poteto <h3.poteto@gmail.com>
2015-12-07 00:09:45 +0900

    refs #130 db接続情報をdbconf.ymlに統一する

--- models/db/db.go
+++ models/db/db.go
@@ -1,8 +1,11 @@
 package db
+
 import (
-	"os"
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"os"
 )
 
 type DB interface {
@@ -13,10 +16,23 @@ type Database struct {
 }
 
 func (u *Database) Init() *sql.DB {
-	username := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	database := os.Getenv("DB_NAME")
-	db, err := sql.Open("mysql", username + ":" + password + "@/" + database + "?charset=utf8")
+	env := os.Getenv("GOJIENV")
+	buf, err := ioutil.ReadFile("db/dbconf.yml")
+	if err != nil {
+		panic(err)
+	}
+	m := make(map[interface{}]interface{})
+	err = yaml.Unmarshal(buf, &m)
+	if err != nil {
+		panic(err)
+	}
+	username := m[env].(map[interface{}]interface{})["user"].(string)
+	password := m[env].(map[interface{}]interface{})["password"].(string)
+	database := m[env].(map[interface{}]interface{})["name"].(string)
+	username = os.ExpandEnv(username)
+	password = os.ExpandEnv(password)
+	database = os.ExpandEnv(database)
+	db, err := sql.Open("mysql", username+":"+password+"@/"+database+"?charset=utf8")
 	if err != nil {
 		panic(err.Error())
 	}
