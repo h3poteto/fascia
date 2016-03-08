8e40fdbec2c5e508298a9e808983a4a1276a6225
h3poteto <h3.poteto@gmail.com>
2016-03-09 01:25:03 +0900

    refs #13 メール送信部分だけ共通関数に切り出す

--- mailers/password_mailer/password_mailer.go
+++ mailers/password_mailer/password_mailer.go
@@ -12,47 +12,13 @@ import (
 )
 
 func Reset(id int64, address string, token string) {
-	if test() {
-		return
-	}
-	if !production() {
-		address = config.Element("mail").(map[interface{}]interface{})["to"].(string)
-	}
 	domain := config.Element("fqdn").(string)
 	resetURL := fmt.Sprintf("http://%s/passwords/%d/edit?token=%s", domain, id, token)
+	title := "Password reseted"
+	rawBody := "Your password is reseted. Please access to following URL, and set new password. \n " + resetURL + " \n This URL is valid for 24 hours."
+	htmlBody := "<p>Your password is reseted.</p><p>Please access to following URL, and set new password. </p><p><a href='" + resetURL + "'>" + resetURL + "</a></p><p>This URL is valid for 24 hours.</p>"
+	resp, err := sendMail(address, title, htmlBody, rawBody)
 
-	svc := ses.New(session.New())
-
-	params := &ses.SendEmailInput{
-		Destination: &ses.Destination{
-			BccAddresses: []*string{},
-			CcAddresses:  []*string{},
-			ToAddresses: []*string{
-				aws.String(address),
-			},
-		},
-		Message: &ses.Message{
-			Body: &ses.Body{
-				Html: &ses.Content{
-					Data:    aws.String("<p>Your password is reseted.</p><p>Please access to following URL, and set new password. </p><p><a href='" + resetURL + "'>" + resetURL + "</a></p><p>This URL is valid for 24 hours.</p>"),
-					Charset: aws.String("UTF-8"),
-				},
-				Text: &ses.Content{
-					Data:    aws.String("Your password is reseted. Please access to following URL, and set new password. \n " + resetURL + " \n This URL is valid for 24 hours."),
-					Charset: aws.String("UTF-8"),
-				},
-			},
-			Subject: &ses.Content{
-				Data:    aws.String("Password reseted"),
-				Charset: aws.String("UTF-8"),
-			},
-		},
-		Source: aws.String(config.Element("mail").(map[interface{}]interface{})["from"].(string)),
-		ReplyToAddresses: []*string{
-			aws.String(config.Element("mail").(map[interface{}]interface{})["from"].(string)),
-		},
-	}
-	resp, err := svc.SendEmail(params)
 	if err != nil {
 		logging.SharedInstance().MethodInfo("PasswordMailer", "Reset", true).Errorf("send mail error: %v", err)
 		return
@@ -81,4 +47,47 @@ func test() bool {
 	} else {
 		return true
 	}
+}
+
+func sendMail(address string, title string, htmlBody string, rawBody string) (r *ses.SendEmailOutput, e error) {
+	if test() {
+		return nil, nil
+	}
+	if !production() {
+		address = config.Element("mail").(map[interface{}]interface{})["to"].(string)
+	}
+
+	svc := ses.New(session.New())
+
+	params := &ses.SendEmailInput{
+		Destination: &ses.Destination{
+			BccAddresses: []*string{},
+			CcAddresses:  []*string{},
+			ToAddresses: []*string{
+				aws.String(address),
+			},
+		},
+		Message: &ses.Message{
+			Body: &ses.Body{
+				Html: &ses.Content{
+					Data:    aws.String(htmlBody),
+					Charset: aws.String("UTF-8"),
+				},
+				Text: &ses.Content{
+					Data:    aws.String(rawBody),
+					Charset: aws.String("UTF-8"),
+				},
+			},
+			Subject: &ses.Content{
+				Data:    aws.String(title),
+				Charset: aws.String("UTF-8"),
+			},
+		},
+		Source: aws.String(config.Element("mail").(map[interface{}]interface{})["from"].(string)),
+		ReplyToAddresses: []*string{
+			aws.String(config.Element("mail").(map[interface{}]interface{})["from"].(string)),
+		},
+	}
+	resp, err := svc.SendEmail(params)
+	return resp, err
 }
