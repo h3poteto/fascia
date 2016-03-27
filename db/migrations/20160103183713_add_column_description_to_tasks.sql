0ed7356480db0a517b8d0335c9ced330760b6e01
h3poteto <h3.poteto@gmail.com>
2016-03-27 14:52:11 +0900

    fix mysql5.7以降ではtext型カラムにデフォルト値の指定ができないことへの対応

--- db/migrations/20160103183713_add_column_description_to_tasks.sql
+++ db/migrations/20160103183713_add_column_description_to_tasks.sql
@@ -1,7 +1,7 @@
 
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
-ALTER TABLE tasks ADD COLUMN description text NOT NULL DEFAULT "" AFTER title;
+ALTER TABLE tasks ADD COLUMN description text NOT NULL AFTER title;
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
