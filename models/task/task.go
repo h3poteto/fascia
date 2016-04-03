6d2a6aaeb4e21d39cb82d2a8846bb6e18fcbbcd3
h3poteto <h3.poteto@gmail.com>
2016-04-04 02:21:46 +0900

    refs #115 task#save時のrescueでerrorを正確に返す

--- models/task/task.go
+++ models/task/task.go
@@ -9,6 +9,7 @@ import (
 	"../repository"
 	"database/sql"
 	"errors"
+	"runtime"
 )
 
 type Task interface {
@@ -92,7 +93,14 @@ func (u *TaskStruct) Save(repo *repository.RepositoryStruct, OauthToken *sql.Nul
 	defer func() {
 		if err := recover(); err != nil {
 			transaction.Rollback()
-			e = errors.New("unexpected error")
+			switch ty := err.(type) {
+			case runtime.Error:
+				e = ty
+			case string:
+				e = errors.New(err.(string))
+			default:
+				e = errors.New("unexpected error")
+			}
 		}
 	}()
 
