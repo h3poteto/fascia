7372ce00ade1ccc3a9c10870714febfc444a0a2f
h3poteto <h3.poteto@gmail.com>
2016-06-08 01:31:19 +0900

    refs #151 編集可能かどうかのフラグをInitListかどうかの判定に変更

--- controllers/lists_controller.go
+++ controllers/lists_controller.go
@@ -35,7 +35,7 @@ type ListJSONFormat struct {
 	Color        string
 	ListOptionID int64
 	IsHidden     bool
-	IsEditable   bool
+	IsInitList   bool
 }
 
 type AllListJSONFormat struct {
@@ -315,5 +315,5 @@ func ListsFormatToJSON(lists []*listModel.ListStruct) []*ListJSONFormat {
 
 // ListFormatToJSON convert a list model to json
 func ListFormatToJSON(list *listModel.ListStruct) *ListJSONFormat {
-	return &ListJSONFormat{ID: list.ID, ProjectID: list.ProjectID, UserID: list.UserID, Title: list.Title.String, ListTasks: TaskFormatToJSON(list.Tasks()), Color: list.Color.String, ListOptionID: list.ListOptionID.Int64, IsHidden: list.IsHidden, IsEditable: list.IsInitList()}
+	return &ListJSONFormat{ID: list.ID, ProjectID: list.ProjectID, UserID: list.UserID, Title: list.Title.String, ListTasks: TaskFormatToJSON(list.Tasks()), Color: list.Color.String, ListOptionID: list.ListOptionID.Int64, IsHidden: list.IsHidden, IsInitList: list.IsInitList()}
 }
