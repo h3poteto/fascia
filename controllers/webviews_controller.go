3423c05f012452d36cf29371ee41c97d71ce345a
h3poteto <h3.poteto@gmail.com>
2016-06-12 23:52:28 +0900

    refs #115 webviews_controllerのlogging#MethodInfo呼び出しを修正

--- controllers/webviews_controller.go
+++ controllers/webviews_controller.go
@@ -6,13 +6,15 @@ import (
 	"../modules/logging"
 	"time"
 
+	"html/template"
+	"net/http"
+
 	"github.com/flosch/pongo2"
 	"github.com/goji/param"
 	"github.com/gorilla/sessions"
+	"github.com/pkg/errors"
 	"github.com/zenazn/goji/web"
 	"golang.org/x/oauth2"
-	"html/template"
-	"net/http"
 )
 
 // Webviews controller struct
@@ -25,7 +27,7 @@ func (u *Webviews) SignIn(c web.C, w http.ResponseWriter, r *http.Request) {
 
 	token, err := GenerateCSRFToken(c, w, r)
 	if err != nil {
-		logging.SharedInstance().MethodInfo("WebviewsController", "SignIn", true, c).Errorf("CSRF error: %v", err)
+		logging.SharedInstance().MethodInfoWithStacktrace("WebviewsController", "SignIn", err, c).Errorf("CSRF error: %v", err)
 		InternalServerError(w, r)
 		return
 	}
@@ -41,7 +43,8 @@ func (u *Webviews) SignIn(c web.C, w http.ResponseWriter, r *http.Request) {
 
 	tpl, err := pongo2.DefaultSet.FromFile("webviews/sign_in.html.tpl")
 	if err != nil {
-		logging.SharedInstance().MethodInfo("WebviewsController", "SignIn", true, c).Errorf("template error: %v", err)
+		err := errors.Wrap(err, "template error")
+		logging.SharedInstance().MethodInfoWithStacktrace("WebviewsController", "SignIn", err, c).Error(err)
 		InternalServerError(w, r)
 		return
 	}
@@ -53,54 +56,60 @@ func (u *Webviews) SignIn(c web.C, w http.ResponseWriter, r *http.Request) {
 func (u *Webviews) NewSession(c web.C, w http.ResponseWriter, r *http.Request) {
 	session, err := cookieStore.Get(r, "fascia")
 	if err != nil {
-		logging.SharedInstance().MethodInfo("WebviewsController", "NewSession", true, c).Errorf("get session error: %v", err)
+		err := errors.Wrap(err, "session error")
+		logging.SharedInstance().MethodInfoWithStacktrace("WebviewsController", "NewSession", err, c).Error(err)
 		InternalServerError(w, r)
 		return
 	}
 	session.Options = &sessions.Options{MaxAge: -1}
 	err = session.Save(r, w)
 	if err != nil {
-		logging.SharedInstance().MethodInfo("WebviewsController", "NewSession", true, c).Errorf("save session error: %v", err)
+		err := errors.Wrap(err, "session error")
+		logging.SharedInstance().MethodInfoWithStacktrace("WebviewsController", "NewSession", err, c).Error(err)
 		InternalServerError(w, r)
 		return
 	}
 	err = r.ParseForm()
 	if err != nil {
-		logging.SharedInstance().MethodInfo("WebviewsController", "NewSession", true, c).Errorf("wrong form: %v", err)
+		err := errors.Wrap(err, "wrong form")
+		logging.SharedInstance().MethodInfoWithStacktrace("WebviewsController", "NewSession", err, c).Error(err)
 		BadRequest(w, r)
 		return
 	}
 	var signInForm SignInForm
 	err = param.Parse(r.PostForm, &signInForm)
 	if err != nil {
-		logging.SharedInstance().MethodInfo("WebviewsController", "NewSession", true, c).Errorf("wrong parameter: %v", err)
+		err := errors.Wrap(err, "wrong parameter")
+		logging.SharedInstance().MethodInfoWithStacktrace("WebviewsController", "NewSession", err, c).Error(err)
 		InternalServerError(w, r)
 		return
 	}
 
 	if !CheckCSRFToken(r, signInForm.Token) {
-		logging.SharedInstance().MethodInfo("WebviewsController", "NewSession", true, c).Error("cannot verify CSRF token")
+		err := errors.New("cannot verify CSRF token")
+		logging.SharedInstance().MethodInfoWithStacktrace("WebviewsController", "NewSession", err, c).Error(err)
 		InternalServerError(w, r)
 		return
 	}
 
 	currentUser, err := userModel.Login(template.HTMLEscapeString(signInForm.Email), template.HTMLEscapeString(signInForm.Password))
 	if err != nil {
-		logging.SharedInstance().MethodInfo("WebviewsController", "NewSession", false, c).Infof("login error: %v", err)
+		logging.SharedInstance().MethodInfo("WebviewsController", "NewSession", c).Infof("login error: %v", err)
 		http.Redirect(w, r, "/webviews/sign_in", 302)
 		return
 	}
-	logging.SharedInstance().MethodInfo("WebviewsController", "NewSession", false, c).Debugf("login success: %+v", currentUser)
+	logging.SharedInstance().MethodInfo("WebviewsController", "NewSession", c).Debugf("login success: %+v", currentUser)
 	session, err = cookieStore.Get(r, "fascia")
 	session.Options = &sessions.Options{Path: "/", MaxAge: config.Element("session").(map[interface{}]interface{})["timeout"].(int)}
 	session.Values["current_user_id"] = currentUser.ID
 	err = session.Save(r, w)
 	if err != nil {
-		logging.SharedInstance().MethodInfo("WebviewsController", "NewSessions", true, c).Errorf("session error: %v", err)
+		err := errors.Wrap(err, "session error")
+		logging.SharedInstance().MethodInfoWithStacktrace("WebviewsController", "NewSessions", err, c).Error(err)
 		InternalServerError(w, r)
 		return
 	}
-	logging.SharedInstance().MethodInfo("WebviewsController", "NewSession", false, c).Info("login success")
+	logging.SharedInstance().MethodInfo("WebviewsController", "NewSession", c).Info("login success")
 	http.Redirect(w, r, "/webviews/callback", 302)
 	return
 }
