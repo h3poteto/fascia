569f9508bde058b3df2d74c9a040facdc77535e6
h3poteto <h3.poteto@gmail.com>
2016-07-05 00:49:48 +0900

    refs #141 ShowTaskModalのcomponentテストを追加

---
+++ test/components/ListView/ShowTaskModalTest.js
@@ -0,0 +1,35 @@
+import TestUtils from 'react-addons-test-utils'
+import expect from 'expect'
+import React from 'react'
+import ShowTaskModal from '../../../frontend/javascripts/components/ListView/ShowTaskModal.jsx'
+import * as ShowTaskModalFixture from '../../fixtures/components/ListView/ShowTaskModalFixture'
+
+function setup(props) {
+  let renderer = TestUtils.createRenderer()
+  renderer.render(<ShowTaskModal {...props} />)
+  let output = renderer.getRenderOutput()
+
+  return {
+    props,
+    output,
+    renderer
+  }
+}
+
+describe('ListView::ShowTaskModal', () => {
+  context('when task modal close', () => {
+    let state = ShowTaskModalFixture.initState()
+    it('should not render show task modal', () => {
+      const { output } = setup(state)
+      expect(output.props.isOpen).toBe(false)
+    })
+  })
+  context('when task modal open', () => {
+    let state = ShowTaskModalFixture.openShowTaskModalState()
+    it('should render show task modal', () => {
+      const { output } = setup(state)
+      expect(output.props.isOpen).toBe(true)
+    })
+  })
+}
+)
