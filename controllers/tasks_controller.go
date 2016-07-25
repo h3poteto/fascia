01e9e0b7200d337790a5f26b69b5d27c7f4f974c
h3poteto <h3.poteto@gmail.com>
2016-07-26 01:25:53 +0900

    refs #223 TasksController#MoveTaskのログイン処理後メソッドを共通化

--- controllers/tasks_controller.go
+++ controllers/tasks_controller.go
@@ -198,46 +198,28 @@ func (u *Tasks) MoveTask(c web.C, w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "not logined", 401)
 		return
 	}
-	encoder := json.NewEncoder(w)
-	projectID, err := strconv.ParseInt(c.URLParams["project_id"], 10, 64)
+
+	parentProject, parentList, statusCode, err := setProjectAndList(c, w, currentUser)
 	if err != nil {
-		err := errors.Wrap(err, "parse error")
 		logging.SharedInstance().MethodInfoWithStacktrace("TasksController", "MoveTask", err, c).Error(err)
-		http.Error(w, "project not found", 404)
-		return
-	}
-	parentProject, err := projectModel.FindProject(projectID)
-	if err != nil || parentProject.UserID != currentUser.ID {
-		logging.SharedInstance().MethodInfo("TasksController", "MoveTask", c).Warnf("project not found: %v", err)
-		http.Error(w, "project not found", 404)
+		switch statusCode {
+		case 404:
+			http.Error(w, "Not Found", 404)
+		default:
+			http.Error(w, "Internal Server Error", 500)
+		}
 		return
 	}
 
-	listID, err := strconv.ParseInt(c.URLParams["list_id"], 10, 64)
+	task, statusCode, err := setTask(c, w, parentList)
 	if err != nil {
-		err := errors.Wrap(err, "parse error")
 		logging.SharedInstance().MethodInfoWithStacktrace("TasksController", "MoveTask", err, c).Error(err)
-		http.Error(w, "list not found", 404)
-		return
-	}
-	parentList, err := listModel.FindList(parentProject.ID, listID)
-	if err != nil {
-		logging.SharedInstance().MethodInfo("TasksController", "MoveTask", c).Warnf("list not found: %v", err)
-		http.Error(w, "list not found", 404)
-		return
-	}
-
-	taskID, err := strconv.ParseInt(c.URLParams["task_id"], 10, 64)
-	if err != nil {
-		err := errors.Wrap(err, "parse error")
-		logging.SharedInstance().MethodInfoWithStacktrace("TasksController", "MoveTask", err, c).Error(err)
-		http.Error(w, "task not found", 404)
-		return
-	}
-	task, err := taskModel.FindTask(parentList.ID, taskID)
-	if err != nil {
-		logging.SharedInstance().MethodInfoWithStacktrace("TasksController", "MoveTask", err, c).Errorf("find task error: %v", err)
-		http.Error(w, "task not find", 500)
+		switch statusCode {
+		case 404:
+			http.Error(w, "Not Found", 404)
+		default:
+			http.Error(w, "Internal Server Error", 500)
+		}
 		return
 	}
 
@@ -276,6 +258,9 @@ func (u *Tasks) MoveTask(c web.C, w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "failed change list", 500)
 		return
 	}
+
+	encoder := json.NewEncoder(w)
+
 	allLists, err := parentProject.Lists()
 	if err != nil {
 		logging.SharedInstance().MethodInfoWithStacktrace("TasksController", "MoveTask", err, c).Error(err)
