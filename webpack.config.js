0b48d0165d1b65dc1dce9de7e9c696d8963ed7fd
h3poteto <h3.poteto@gmail.com>
2017-02-12 22:54:08 +0900

    refs #305 [eslint] Fix quotation in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,24 @@
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var CopyWebpackPlugin = require("copy-webpack-plugin");
-var subscript = require('markdown-it-sub');
-var superscript = require('markdown-it-sup');
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-  devtool: "source-map",
+  devtool: 'source-map',
   entry: {
-    'javascripts/bundle.js': "./frontend/javascripts/bundle.js",
-    'stylesheets/application.css': "./frontend/stylesheets/application.scss",
-    'stylesheets/application-webview.css': "./frontend/stylesheets/application-webview.scss",
+    'javascripts/bundle.js': './frontend/javascripts/bundle.js',
+    'stylesheets/application.css': './frontend/stylesheets/application.scss',
+    'stylesheets/application-webview.css': './frontend/stylesheets/application-webview.scss',
   },
   output: {
-    path: "./public/assets",
-    filename: "[name]"
+    path: './public/assets',
+    filename: '[name]'
   },
 
   resolve: {
     modulesDirectories: [
-      __dirname + "/fronted/javascripts",
-      __dirname + "/node_modules"
+      __dirname + '/fronted/javascripts',
+      __dirname + '/node_modules'
     ],
-    extensions: ["", ".js", ".jsx"]
+    extensions: ['', '.js', '.jsx']
   },
 
   module: {
@@ -28,32 +26,32 @@ module.exports = {
       {
         test: /\.js[x]?$/,
         exclude: /node_modules/,
-        loader: ["babel"],
+        loader: ['babel'],
         query: {}
       },
       {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract("style", "css")
+        loader: ExtractTextPlugin.extract('style', 'css')
       },
       {
         test: /\.scss$/,
-        loader: ExtractTextPlugin.extract("style", "css!sass")
+        loader: ExtractTextPlugin.extract('style', 'css!sass')
       },
       {
         test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&mimetype=application/font-woff"
+        loader: 'url?limit=10000&mimetype=application/font-woff'
       },
       {
         test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&mimetype=application/octet-stream"
+        loader: 'url?limit=10000&mimetype=application/octet-stream'
       },
       {
         test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "file"
+        loader: 'file'
       },
       {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&mimetype=image/svg+xml"
+        loader: 'url?limit=10000&mimetype=image/svg+xml'
       },
       {
         test: /\.json/,
@@ -62,7 +60,7 @@ module.exports = {
     ]
   },
   plugins: [
-    new ExtractTextPlugin("[name]"),
-    new CopyWebpackPlugin([{ from: "./frontend/images", to: "./images" }])
+    new ExtractTextPlugin('[name]'),
+    new CopyWebpackPlugin([{ from: './frontend/images', to: './images' }])
   ]
 }
