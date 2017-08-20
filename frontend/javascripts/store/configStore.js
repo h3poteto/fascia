097c309f70e8755f0197df846211ceb07d903d97
AkiraFukushima <h3.poteto@gmail.com>
2017-08-20 21:15:11 +0900

    refs #373 Update node packages

--- frontend/javascripts/store/configStore.js
+++ frontend/javascripts/store/configStore.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from '../reducers'
 import thunk from 'redux-thunk'
-import createLogger from 'redux-logger'
+import { createLogger } from 'redux-logger'
 
 const logger = createLogger()
 const createAppStore = compose(
