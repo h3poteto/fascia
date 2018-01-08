4bc89d93c5a397500fc97cdb2b75339910cbf110
AkiraFukushima <h3.poteto@gmail.com>
2018-01-08 23:13:39 +0900

    Fix inquiry entity using reflect and reload

--- server/commands/contact/inquiry.go
+++ server/commands/contact/inquiry.go
@@ -18,10 +18,9 @@ func InitCreateInquiry(id int64, email, name, message string) *CreateInquiry {
 
 // Run save a inquiry entity.
 func (i *CreateInquiry) Run() error {
-	entity, err := i.InquiryEntity.Save()
+	err := i.InquiryEntity.Save()
 	if err != nil {
 		return err
 	}
-	i.InquiryEntity = entity
 	return nil
 }
--- server/entities/inquiry/inquiry.go
+++ server/entities/inquiry/inquiry.go
@@ -6,28 +6,44 @@ import (
 
 // Inquiry is a entity for inquiry.
 type Inquiry struct {
-	ID      int64
-	Email   string
-	Name    string
-	Message string
+	ID             int64
+	Email          string
+	Name           string
+	Message        string
+	infrastructure *inquiry.Inquiry
 }
 
 // New returns a inquiry struct.
 func New(id int64, email, name, message string) *Inquiry {
-	return &Inquiry{
-		id,
-		email,
-		name,
-		message,
+	infrastructure := inquiry.New(id, email, name, message)
+	i := &Inquiry{
+		infrastructure: infrastructure,
 	}
+	i.reload()
+	return i
+}
+
+func (i *Inquiry) reflect() {
+	i.infrastructure.ID = i.ID
+	i.infrastructure.Email = i.Email
+	i.infrastructure.Name = i.Name
+	i.infrastructure.Message = i.Message
+}
+
+func (i *Inquiry) reload() error {
+	i.ID = i.infrastructure.ID
+	i.Email = i.infrastructure.Email
+	i.Name = i.infrastructure.Name
+	i.Message = i.infrastructure.Message
+	return nil
 }
 
 // Save a inquiry entity, and returns the latest inquiry object.
-func (i *Inquiry) Save() (*Inquiry, error) {
-	record := inquiry.New(i.ID, i.Email, i.Name, i.Message)
-	err := record.Save()
+func (i *Inquiry) Save() error {
+	i.reflect()
+	err := i.infrastructure.Save()
 	if err != nil {
-		return nil, err
+		return err
 	}
-	return New(record.ID, record.Email, record.Name, record.Message), nil
+	return i.reload()
 }
