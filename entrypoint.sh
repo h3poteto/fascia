f0d33f0d15eed8102e505a792d34a5814768dc0c
AkiraFukushima <h3.poteto@gmail.com>
2019-02-06 23:04:30 +0900

    Exit entrypoint when does not have aws credentials

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
+
 export AWS_DEFAULT_REGION=ap-northeast-1
+# Confirm aws credential to stop the container when the container is created.
+myaws ssm parameter get fascia.$SERVICE_ENV.database_url --region $AWS_DEFAULT_REGION
+ret=$?
+if [[ ret -ne 0 ]]; then
+    exit
+fi
+
 export DATABASE_URL=`myaws ssm parameter get fascia.$SERVICE_ENV.database_url --region $AWS_DEFAULT_REGION`
 export DB_USER=`myaws ssm parameter get fascia.$SERVICE_ENV.db_user --region $AWS_DEFAULT_REGION`
 export DB_NAME=`myaws ssm parameter get fascia.$SERVICE_ENV.db_name --region $AWS_DEFAULT_REGION`
