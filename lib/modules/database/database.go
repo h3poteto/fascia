2a0764f3da60097cfd920bac0a7d2e4e8b91ad26
AkiraFukushima <h3.poteto@gmail.com>
2019-09-19 23:53:20 +0900

    refs #461 Use postgresql as default database

--- lib/modules/database/database.go
+++ lib/modules/database/database.go
@@ -4,25 +4,20 @@ import (
 	"database/sql"
 	"io/ioutil"
 	"os"
-	"path/filepath"
 
-	// mysql is used in database connection, it is slient include
-	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
 	"gopkg.in/yaml.v2"
 )
 
-// Database has database connection
 type Database struct {
 	Connection *sql.DB
 }
 
 var sharedInstance = New()
 
-// New is create Database object with connection pool
 func New() *Database {
 	env := os.Getenv("APPENV")
-	root := os.Getenv("APPROOT")
-	path := filepath.Join(root, "db/dbconf.yml")
+	path := "db/dbconf.yml"
 	buf, err := ioutil.ReadFile(path)
 	if err != nil {
 		panic(err)
@@ -32,21 +27,16 @@ func New() *Database {
 	if err != nil {
 		panic(err)
 	}
-	username := m[env].(map[interface{}]interface{})["user"].(string)
-	password := m[env].(map[interface{}]interface{})["password"].(string)
-	database := m[env].(map[interface{}]interface{})["name"].(string)
-	host := m[env].(map[interface{}]interface{})["host"].(string)
+	open := m[env].(map[interface{}]interface{})["open"].(string)
 	pool := m[env].(map[interface{}]interface{})["pool"].(int)
-	username = os.ExpandEnv(username)
-	password = os.ExpandEnv(password)
-	database = os.ExpandEnv(database)
-	host = os.ExpandEnv(host)
-	db, err := sql.Open("mysql", username+":"+password+"@tcp("+host+":3306)/"+database+"?parseTime=true&charset=utf8mb4")
+	open = os.ExpandEnv(open)
+
+	db, err := sql.Open("postgres", open)
 	if err != nil {
 		panic(err)
 	}
 
-	// MaxIdle: mysqlへのアクセスがないときにも保持しておくconnection poolの上限
+	// MaxIdle: dbへのアクセスがないときにも保持しておくconnection poolの上限
 	// MaxOpen: idle + activeなconnection poolの上限数
 	db.SetMaxIdleConns(pool)
 	db.SetMaxOpenConns(pool)
@@ -56,12 +46,10 @@ func New() *Database {
 	}
 }
 
-// SharedInstance return database connection object
 func SharedInstance() *Database {
 	return sharedInstance
 }
 
-// Close database connection
 func (d *Database) Close() error {
 	return d.Connection.Close()
 }
