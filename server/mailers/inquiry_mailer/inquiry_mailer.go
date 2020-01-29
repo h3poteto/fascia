ad2be7f54aca8bad29a7ab82be0d1d4118c0621a
AkiraFukushima <h3.poteto@gmail.com>
2020-01-29 23:44:15 +0900

    refs #581 Use gmail instead of SES for contact notification

--- server/mailers/inquiry_mailer/inquiry_mailer.go
+++ server/mailers/inquiry_mailer/inquiry_mailer.go
@@ -1,15 +1,12 @@
 package inquiry
 
 import (
+	"net/smtp"
 	"os"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ses"
 	"github.com/h3poteto/fascia/config"
 	"github.com/h3poteto/fascia/lib/modules/logging"
 	"github.com/h3poteto/fascia/server/domains/inquiry"
-	"github.com/pkg/errors"
 )
 
 // Notify sends a email to administrators.
@@ -17,12 +14,11 @@ func Notify(i *inquiry.Inquiry) {
 	title := "You got a inquiry"
 	rawBody := "From: " + i.Email + "\n Name: " + i.Name + "\n Message: " + i.Message
 	htmlBody := "<p>From: " + i.Email + "</p><p>Name: " + i.Name + "</p><p>" + i.Message + "</p>"
-	resp, err := sendMail(title, htmlBody, rawBody)
+	err := sendMail(title, htmlBody, rawBody)
 	if err != nil {
 		logging.SharedInstance().MethodInfoWithStacktrace("InquiryMailer", "Notify", err).Error(err)
 		return
 	}
-	logging.SharedInstance().MethodInfo("InquiryMailer", "Notify").Debugf("send mail response: %v", resp)
 	logging.SharedInstance().MethodInfo("InquiryMailer", "Notify").Info("success to send mail")
 	return
 }
@@ -43,43 +39,16 @@ func test() bool {
 	return false
 }
 
-func sendMail(title, htmlBody, rawBody string) (r *ses.SendEmailOutput, e error) {
+func sendMail(title, htmlBody, rawBody string) error {
 	if test() {
-		return nil, nil
+		return nil
 	}
 
-	address := config.Element("mail").(map[interface{}]interface{})["to"].(string)
+	to := config.Element("mail").(map[interface{}]interface{})["to"].(string)
+	from := config.Element("mail").(map[interface{}]interface{})["from"].(string)
 
-	svc := ses.New(session.New(), config.AWS("us-east-1"))
-	params := &ses.SendEmailInput{
-		Destination: &ses.Destination{
-			BccAddresses: []*string{},
-			CcAddresses:  []*string{},
-			ToAddresses: []*string{
-				aws.String(address),
-			},
-		},
-		Message: &ses.Message{
-			Body: &ses.Body{
-				Html: &ses.Content{
-					Data:    aws.String(htmlBody),
-					Charset: aws.String("UTF-8"),
-				},
-				Text: &ses.Content{
-					Data:    aws.String(rawBody),
-					Charset: aws.String("UTF-8"),
-				},
-			},
-			Subject: &ses.Content{
-				Data:    aws.String(title),
-				Charset: aws.String("UTF-8"),
-			},
-		},
-		Source: aws.String(config.Element("mail").(map[interface{}]interface{})["from"].(string)),
-		ReplyToAddresses: []*string{
-			aws.String(config.Element("mail").(map[interface{}]interface{})["from"].(string)),
-		},
-	}
-	resp, err := svc.SendEmail(params)
-	return resp, errors.Wrap(err, "aws api error")
+	auth := smtp.PlainAuth("", from, os.Getenv("GMAIL_APP_PASSWORD"), "smtp.gmail.com")
+
+	return smtp.SendMail("smtp.gmail.com:587", auth, from, []string{to}, []byte(
+		"To: "+to+"\r\n"+"Subject:"+title+"\r\n\r\n"+rawBody))
 }
