941088a2338cef9a985e928779fbd80ac58cd205
AkiraFukushima <h3.poteto@gmail.com>
2020-02-03 22:31:53 +0900

    refs #599 Rescue nil object when list option is empty

--- server/domains/list/option.go
+++ server/domains/list/option.go
@@ -10,6 +10,9 @@ func NewOption(id int64, action string) *Option {
 
 // IsCloseAction return whether it is close option
 func (o *Option) IsCloseAction() bool {
+	if o == nil {
+		return false
+	}
 	if o.Action == "close" {
 		return true
 	}
