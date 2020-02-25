114349a1877671d1c7bf1d08576ded9f43ee5a34
AkiraFukushima <h3.poteto@gmail.com>
2020-02-26 01:21:06 +0900

    refs #622 Fix menu bar design

--- assets/js/components/menu.tsx
+++ assets/js/components/menu.tsx
@@ -8,19 +8,29 @@ const menu: React.FC<Props> = ({ children }) => {
     <div>
       <header className="top-header">
         <Navbar bg="primary" variant="dark" expand="lg">
-          <Navbar.Brand href="/">Fascia</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="#home">Projects</Nav.Link>
-              <Nav.Link href="#link">Contact</Nav.Link>
-              <NavDropdown title="Accounts" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Settings</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
+          <Navbar.Brand href="/">
+            <img
+              alt=""
+              src="/images/fascia-icon.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            Fascia</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="/">Projects</Nav.Link>
+                <Nav.Link href="/inquiries/new">Contact</Nav.Link>
+              </Nav>
+              <Nav>
+                <NavDropdown title="Accounts" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Settings</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
         </Navbar>
       </header>
       <div className="contents">
