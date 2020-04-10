64f9a1ce38e9cf42f49eb20408403a52d03f5022
AkiraFukushima <h3.poteto@gmail.com>
2020-04-10 22:06:21 +0900

    refs #621 Fix production settings for lp webpack

--- lp/webpack.config.js
+++ lp/webpack.config.js
@@ -1,23 +1,21 @@
-const path = require('path')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const ManifestPlugin = require('webpack-manifest-plugin')
-
-const production = process.env.NODE_ENV === 'production'
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const ManifestPlugin = require("webpack-manifest-plugin");
 
 module.exports = {
   entry: {
-    'css/lp': path.join(__dirname, './css/lp.scss'),
-    'css/lp-webview': path.join(__dirname, './css/lp-webview.scss')
+    "css/lp": path.join(__dirname, "./css/lp.scss"),
+    "css/lp-webview": path.join(__dirname, "./css/lp-webview.scss")
   },
   output: {
-    path: path.resolve(__dirname, '../public/lp'),
-    filename: '[name].js'
+    path: path.resolve(__dirname, "../public/lp"),
+    filename: "[name].js"
   },
   cache: true,
   resolve: {
-    modules: [path.resolve(__dirname, './node_modules')],
-    extensions: ['*', '.css', '.scss']
+    modules: [path.resolve(__dirname, "./node_modules")],
+    extensions: ["*", ".css", ".scss"]
   },
   module: {
     rules: [
@@ -28,28 +26,28 @@ module.exports = {
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
-              publicPath: path.resolve(__dirname, '../public/lp')
+              publicPath: path.resolve(__dirname, "../public/lp")
             }
           },
-          'css-loader',
-          'sass-loader'
+          "css-loader",
+          "sass-loader"
         ]
       },
       {
         test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+        loader: "url-loader?limit=10000&mimetype=application/font-woff"
       },
       {
         test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+        loader: "url-loader?limit=10000&mimetype=application/octet-stream"
       },
       {
         test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file-loader'
+        loader: "file-loader"
       },
       {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+        loader: "url-loader?limit=10000&mimetype=image/svg+xml"
       }
     ]
   },
@@ -58,9 +56,14 @@ module.exports = {
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
-      filename: production ? '[name].[hash].css' : '[name].css',
-      chunkFilename: production ? '[id].[hash].css' : '[id].css'
+      filename: "[name].css",
+      chunkFilename: "[id].css"
     }),
-    new CopyWebpackPlugin([{ from: path.resolve(__dirname, './images'), to: path.resolve(__dirname, '../public/lp/images') }])
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, "./images"),
+        to: path.resolve(__dirname, "../public/lp/images")
+      }
+    ])
   ]
-}
+};
