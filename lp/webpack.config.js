bc433db9acbcb00d9adf74af29b5f79905115da0
AkiraFukushima <h3.poteto@gmail.com>
2020-06-16 01:03:53 +0900

    Fix copy-webpack-plugin options for lp

--- lp/webpack.config.js
+++ lp/webpack.config.js
@@ -6,16 +6,16 @@ const ManifestPlugin = require("webpack-manifest-plugin");
 module.exports = {
   entry: {
     "css/lp": path.join(__dirname, "./css/lp.scss"),
-    "css/lp-webview": path.join(__dirname, "./css/lp-webview.scss")
+    "css/lp-webview": path.join(__dirname, "./css/lp-webview.scss"),
   },
   output: {
     path: path.resolve(__dirname, "../public/lp"),
-    filename: "[name].js"
+    filename: "[name].js",
   },
   cache: true,
   resolve: {
     modules: [path.resolve(__dirname, "./node_modules")],
-    extensions: ["*", ".css", ".scss"]
+    extensions: ["*", ".css", ".scss"],
   },
   module: {
     rules: [
@@ -26,30 +26,30 @@ module.exports = {
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
-              publicPath: path.resolve(__dirname, "../public/lp")
-            }
+              publicPath: path.resolve(__dirname, "../public/lp"),
+            },
           },
           "css-loader",
-          "sass-loader"
-        ]
+          "sass-loader",
+        ],
       },
       {
         test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url-loader?limit=10000&mimetype=application/font-woff"
+        loader: "url-loader?limit=10000&mimetype=application/font-woff",
       },
       {
         test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url-loader?limit=10000&mimetype=application/octet-stream"
+        loader: "url-loader?limit=10000&mimetype=application/octet-stream",
       },
       {
         test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "file-loader"
+        loader: "file-loader",
       },
       {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url-loader?limit=10000&mimetype=image/svg+xml"
-      }
-    ]
+        loader: "url-loader?limit=10000&mimetype=image/svg+xml",
+      },
+    ],
   },
   plugins: [
     new ManifestPlugin(),
@@ -57,13 +57,15 @@ module.exports = {
       // Options similar to the same options in webpackOptions.output
       // both options are optional
       filename: "[name].css",
-      chunkFilename: "[id].css"
+      chunkFilename: "[id].css",
     }),
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(__dirname, "./images"),
-        to: path.resolve(__dirname, "../public/lp/images")
-      }
-    ])
-  ]
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "./images"),
+          to: path.resolve(__dirname, "../public/lp/images"),
+        },
+      ],
+    }),
+  ],
 };
