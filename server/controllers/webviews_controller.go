ed1eec100f03c0cb73d84c32e6c5be966807e8a0
AkiraFukushima <h3.poteto@gmail.com>
2020-06-22 23:57:07 +0900

    refs #796 Add reCAPTCHA in webview contact form

--- server/controllers/webviews_controller.go
+++ server/controllers/webviews_controller.go
@@ -1,12 +1,18 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
 	"github.com/h3poteto/fascia/lib/modules/logging"
 	mailer "github.com/h3poteto/fascia/server/mailers/inquiry_mailer"
 	"github.com/h3poteto/fascia/server/usecases/contact"
 	"github.com/h3poteto/fascia/server/validators"
 
 	"net/http"
+	"net/url"
 	"time"
 
 	"github.com/labstack/echo/v4"
@@ -76,6 +82,39 @@ func (u *Webviews) Inquiry(c echo.Context) error {
 		})
 	}
 
+	val := url.Values{}
+	val.Add("secret", os.Getenv("RECAPTCHA_SECRET_KEY"))
+	val.Add("response", newInquiryFrom.RecaptchaResponse)
+	resp, err := http.PostForm(RecaptchaSiteverifyURL, val)
+	if err != nil {
+		logging.SharedInstance().ControllerWithStacktrace(err, c).Error(err)
+		return err
+	}
+	defer resp.Body.Close()
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	jsonBytes := ([]byte)(byteArray)
+	result := new(RecaptchaResult)
+	if err := json.Unmarshal(jsonBytes, result); err != nil {
+		logging.SharedInstance().ControllerWithStacktrace(err, c).Error(err)
+		return err
+	}
+	if !result.Success || result.RecaptchaSuccess == nil {
+		err := fmt.Errorf("Recaptcha failed: %v", result.RecaptchaFailed.ErrorCodes)
+		logging.SharedInstance().ControllerWithStacktrace(err, c).Warn(err)
+		return c.Render(http.StatusUnprocessableEntity, "webviews/inquiries/new.html.tpl", map[string]interface{}{
+			"title": "Contact",
+			"error": "Session error",
+		})
+	}
+	if result.RecaptchaSuccess != nil && result.RecaptchaSuccess.Score < 0.5 {
+		err := errors.New("Recaptcha score is too low")
+		logging.SharedInstance().ControllerWithStacktrace(err, c).Warn(err)
+		return c.Render(http.StatusUnprocessableEntity, "webviews/inquiries/new.html.tpl", map[string]interface{}{
+			"title": "Contact",
+			"error": "Session error",
+		})
+	}
+
 	inquiry, err := contact.CreateInquiry(newInquiryFrom.Email, newInquiryFrom.Name, newInquiryFrom.Message)
 	if err != nil {
 		logging.SharedInstance().ControllerWithStacktrace(err, c).Error(err)
@@ -83,7 +122,7 @@ func (u *Webviews) Inquiry(c echo.Context) error {
 	}
 	logging.SharedInstance().Controller(c).Info("success to create inquiry")
 
-	// ここでemail送信
+	// Send email
 	go mailer.Notify(inquiry)
 	logging.SharedInstance().Controller(c).Info("success to send inquiry")
 	return c.Render(http.StatusCreated, "webviews/inquiries/create.html.tpl", map[string]interface{}{
